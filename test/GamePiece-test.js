670da3001775e8180bf658b82fb23db5e647b8a2
Justin Stewart <Justinstewart3313@gmail.com>
2018-08-26 14:36:50 -0600

    Run eslint and clean up codeBase

--- test/GamePiece-test.js
+++ test/GamePiece-test.js
@@ -1,50 +1,50 @@
 const { assert } = require('chai');
-const GamePiece = require('../lib/GamePiece.js')
+const GamePiece = require('../lib/GamePiece.js');
 
 describe('GamePiece', () => {
-  let gamepiece;
+	let gamepiece;
 
-  beforeEach(() => {
-    gamepiece = new GamePiece(30, 30, 10, 10, 'green', 1, 1)
-  })
+	beforeEach(() => {
+		gamepiece = new GamePiece(30, 30, 10, 10, 'green', 1, 1);
+	});
 
-  it('should take properties', () => {
-    // Assertion
-    assert.deepEqual(gamepiece, {
-      x: 30,
-      y: 30,
-      height: 10,
-      width: 10,
-      color: 'green',
-      dx: 1,
-      dy: 0,
-      dxv: 1,
-      dyv: 1
-    })
-    // Teardown
-  })
+	it('should take properties', () => {
+		// Assertion
+		assert.deepEqual(gamepiece, {
+			x: 30,
+			y: 30,
+			height: 10,
+			width: 10,
+			color: 'green',
+			dx: 1,
+			dy: 0,
+			dxv: 1,
+			dyv: 1
+		});
+		// Teardown
+	});
 
-  it('should collide with a second gamepiece that occupies the same space', () => {
-    const gamepiece2 = new GamePiece(30, 30, 10, 10, 1, 1, 'green')
+	it('should collide with a second gamepiece that occupies the same space', () => {
+		const gamepiece2 = new GamePiece(30, 30, 10, 10, 1, 1, 'green');
 
-    // Execution
-    const colliding = gamepiece.isCollidingWith(gamepiece2);
+		// Execution
+		const colliding = gamepiece.isCollidingWith(gamepiece2);
 
-    // Assertion
-    assert.isTrue(colliding);
-  })
+		// Assertion
+		assert.isTrue(colliding);
+	});
 
-  it('should not collide with a second gamepiece that does not occupy the same space', () => {
-    const gamepiece2 = new GamePiece(130, 130, 10, 10, 1, 1, 'green')
+	it('should not collide with a second gamepiece that does not occupy the same space', () => {
+		const gamepiece2 = new GamePiece(130, 130, 10, 10, 1, 1, 'green');
 
-    // Execution
-    const colliding = gamepiece.isCollidingWith(gamepiece2);
+		// Execution
+		const colliding = gamepiece.isCollidingWith(gamepiece2);
 
-    // Assertion
-    assert.isFalse(colliding);
-  })
+		// Assertion
+		assert.isFalse(colliding);
+	});
 
-  it('should collide with walls', () => {})
-  it('should be able to move', () => {})
-  it('should be able to changeDirection', () => {})
-})
+	it('should collide with walls', () => {});
+	it('should be able to move', () => {});
+	it('should be able to changeDirection', () => {});
+});
